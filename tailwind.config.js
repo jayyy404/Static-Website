46d1d1f5d2b1a4ecd8353a9ed8c44971379b1101
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2024-09-28 03:47:45 +0800

    All other caches

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // or wherever your files are located
-  ],
+  content: ["./src/**/*.{html,js,css}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
