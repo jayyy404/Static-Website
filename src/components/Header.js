e0eb30087f34196161a1c52951eef6da776e43a6
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2024-09-28 03:46:37 +0800

    Updated Header for more modern clean look

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import bgImage from './pictures/homepage.jpg';
 
 const Header = () => {
     return (
-        <section className="relative min-h-screen w-full bg-cover bg-center boxfit" style={{ backgroundImage: `url(${bgImage})` }}>
-            <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <section 
+            className="relative min-h-screen w-full bg-cover bg-center flex items-center justify-center" 
+            style={{ backgroundImage: `url(${bgImage})` }}
+        >
+            <div className="text-center">
                 <h1 className="text-5xl text-white">WELCOME TO WVSU CANTEEN</h1>
             </div>
-           
         </section>
     );
 };
