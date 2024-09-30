4457866c2e0bf087d578a15c538f38ed7ef87803
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2024-09-30 22:00:13 +0800

    Implemented more aesthetetic changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import bgImage from './pictures/homepage.jpg';
 
 const Header = () => {
@@ -7,8 +8,19 @@ const Header = () => {
             className="relative min-h-screen w-full bg-cover bg-center flex items-center justify-center" 
             style={{ backgroundImage: `url(${bgImage})` }}
         >
-            <div className="text-center">
-                <h1 className="text-5xl text-white">WELCOME TO WVSU CANTEEN</h1>
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+
+            {/* Text Content */}
+            <div className="relative text-center">
+                <h1 className="text-5xl text-white mb-4">WELCOME TO WVSU CANTEEN</h1>
+
+                {/* Minimal Modern Button with hover effect */}
+                <Link to="/Stalls">
+                    <button className="mt-6 px-8 py-3 bg-white text-black font-semibold rounded-full shadow-md transition duration-300 ease-in-out hover:bg-black hover:text-white">
+                        Explore Stalls
+                    </button>
+                </Link>
             </div>
         </section>
     );
